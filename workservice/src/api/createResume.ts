fa893054947cbc50ba6d626604b5daf7de9c1d59
Artsiom <masloartem20@gmail.com>
2024-10-19 16:42:28 +0300

    Resolve problem with loading countrie for select

--- workservice/src/api/createResume.ts
+++ workservice/src/api/createResume.ts
@@ -6,6 +6,6 @@ export interface Country{
 }
 
 export const getCountries = async() : Promise<Country[]> => {
-    const response = await axios.get<Country[]>('https://namaztimes.kz/ru/api/country?type=json')
+    const response = await axios.get('https://namaztimes.kz/ru/api/country?type=json')
     return response.data;
 } 
--- workservice/src/modules/CreateResume/Citizenship.tsx
+++ workservice/src/modules/CreateResume/Citizenship.tsx
@@ -1,48 +1,50 @@
-import React,{ useEffect, useState } from 'react'
-import { getCountries } from '../../api/createResume'
+import React, { useEffect, useState } from 'react';
 import { Country } from '../../api/createResume'
-
-import '../../modules/CreateResume/sass/CreateResume.scss'
-
+import axios from 'axios';
 
 
-const Citizenship:React.FC = () => {
+const Citizenship: React.FC = () => {
     const [citizenship, setCitizenship] = useState<string>('');
     const [countries, setCountries] = useState<Country[]>([]);
 
     const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
         setCitizenship(event.target.value);
-    }
+    };
 
     useEffect(() => {
-        const loadCountries = async() => {
-            const data = await getCountries();
-            setCountries(data);
-            
-        }
+        const loadCountries = async () => {
+            try {
+                const response = await axios.get('https://namaztimes.kz/ru/api/country?type=json');
+                const data = response.data;
 
-        loadCountries()
-    },[])
+                // Преобразуйте данные в массив объектов типа Country
+                const countriesArray: Country[] = Object.entries(data).map(([key, value]) => ({
+                    id: Number(key),
+                    name: value as string,
+                }));
 
-    const countriesArray = Object.entries(countries).map(([key, value]) => ({
-        value: key,
-        label: value,
-    }));
+                setCountries(countriesArray);
+            } catch (error) {
+                console.error('Ошибка при загрузке стран:', error);
+            }
+        };
+
+        loadCountries();
+    }, []);
+
     return (
         <div className='create-resume__citizenship'>
             <div className='create-resume__citizenship__text'>Гражданство</div>
             <select className='create-resume__citizenship__select' name="select" id="options" value={citizenship} onChange={handleChange}>
                 <option value="" disabled>Выберите страну</option>
-                {
-                    countriesArray.map(country => (
-                        <option  key={country.value} value={country.value}>
-                            {country.label}
-                        </option>
-                    ))
-                }
+                {countries.map(country => (
+                    <option key={country.id} value={country.id}>
+                        {country.name}
+                    </option>
+                ))}
             </select>
         </div>
-    )
-}
+    );
+};
 
-export default Citizenship
+export default Citizenship;
